bc3e2c02e44acc043bb25cf695128d0ef4516fdf
Marcin Morawski <marcin@morawskim.pl>
2017-07-08 16:23:52 +0200

    Skeleton file for puppet provider

---
+++ skeletons/puppet_provider.rb
@@ -0,0 +1,17 @@
+Puppet::Type.type(:PUPPET_TYPE_SYMBOL).provide(:PUPPET_PROVIDE_SYMBOL) do
+    desc "Description of this provider"
+
+    commands :command_name => "binary_name"
+
+    def create
+        #todo implement code
+    end
+
+    def destroy
+        #todo implement code
+    end
+
+    def exists?
+        #todo implement code
+    end
+end
